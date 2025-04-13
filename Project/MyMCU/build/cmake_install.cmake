ca048bf650595a8a7572b33839f8b33d529e54cd
joey9D <crstl9D@proton.me>
2025-04-13 20:00:00 +0200

    add cmake/*.cmake files. build successful

--- Project/MyMCU/build/cmake_install.cmake
+++ Project/MyMCU/build/cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -35,6 +35,16 @@ endif()
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/joey/Documents/Programmieren/HardwareAPI/HardwareAPI/Project/MyMCU/build/_deps/spdlog-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/joey/Documents/Programmieren/HardwareAPI/HardwareAPI/Project/MyMCU/build/_deps/cxxopts-build/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
