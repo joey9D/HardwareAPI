048b4bb17e0680b8c45057206083b25985b0a3b5
Kristel <jan.kristel@ws-schaefer.com>
2025-05-21 14:35:47 +0200

    build in new dir

---
+++ Project/MCU_driver_API/build/CMakeFiles/3.28.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19045")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19045")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/MCU_driver_API/cmake/arm_none_eabi_gcc.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
