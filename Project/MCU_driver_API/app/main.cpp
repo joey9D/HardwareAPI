81e0594b48658c47d7e5dd5095e0960852673359
Kristel <jan.kristel@ws-schaefer.com>
2025-07-11 11:02:12 +0200

    update main

--- Project/MCU_driver_API/app/main.cpp
+++ Project/MCU_driver_API/app/main.cpp
@@ -1,19 +1,93 @@
+
+
 #include "main.h"
 
-
-
-
-int main()
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
 {
-    periph::systick::init();
 
-    periph::gpio_stm32c0 led(periph::gpio_stm32c0::port::a, 9, periph::gpio::mode::digital_output);
+	HardwareInterface *hw = HardwareFactory::create();
 
-    while(1)
-    {
-        led.toggle();
-        periph::systick::delay(1000);
-    }
+	hw->init_sys();
 
-    return 0;
+	PinConfig_t led_cfg;
+	led_cfg.pin = 15;
+	led_cfg.port = Port::A;
+	led_cfg.mode = Mode::Output_Push_Pull;
+	led_cfg.pull = Pull::None;
+	led_cfg.speed = Speed::Low;
+	led_cfg.invertedPin = false;
+	led_cfg.debounceTime = 0;
+	led_cfg.debounceState = 0;
+
+
+	PinConfig_t button_cfg;
+	button_cfg.pin = 9;
+	button_cfg.port = Port::A;
+	button_cfg.mode = Mode::Input;
+	button_cfg.pull = Pull::Up;
+	button_cfg.speed = Speed::Low;
+	button_cfg.invertedPin = false;
+	button_cfg.debounceTime = 50;
+	button_cfg.debounceState = 0;
+
+
+	Gpio led(led_cfg);
+	Gpio button(button_cfg);
+
+	led.gpio_init();
+	button.gpio_init();
+
+	bool lastButtonState = button.isDebouncePinOn();
+
+  /* Infinite loop */
+
+  while (1)
+  {
+//	  hw->togglePin();
+//	  hw->delay(500);
+	  bool currentButtonState = button.isDebouncePinOn();
+
+	  if( !lastButtonState && currentButtonState )
+	  {
+		  led.togglePin();
+	  }
+	  lastButtonState = currentButtonState;
+
+  }
 }
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
--- Project/MCU_driver_API/app/main.h
+++ Project/MCU_driver_API/app/main.h
@@ -1,8 +1,60 @@
-#pragma once
 
-#include <iostream>
-#include <cstdint>
-#include "periph/gpio_stm32c0.hpp"
-#include "systick.hpp"
-#include "rcc.hpp"
-#include "FreeRTOSConfig.h"
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "hw_interface.hpp"
+#include "hw_factory.hpp"
+#include "stm32c0_hw.hpp"
+#include "gpio.hpp"
+/* USER CODE END Includes */
+
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define User_Button_Pin GPIO_PIN_13
+#define User_Button_GPIO_Port GPIOC
+#define User_Button_EXTI_IRQn EXTI4_15_IRQn
+#define RCC_OSCX_IN_Pin GPIO_PIN_14
+#define RCC_OSCX_IN_GPIO_Port GPIOC
+#define RCC_OSCX_OUT_Pin GPIO_PIN_15
+#define RCC_OSCX_OUT_GPIO_Port GPIOC
+#define RCC_OSC_IN_Pin GPIO_PIN_0
+#define RCC_OSC_IN_GPIO_Port GPIOF
+#define RCC_OSC_OUT_Pin GPIO_PIN_1
+#define RCC_OSC_OUT_GPIO_Port GPIOF
+#define VCP_USART2_TX_Pin GPIO_PIN_2
+#define VCP_USART2_TX_GPIO_Port GPIOA
+#define VCP_USART2_RX_Pin GPIO_PIN_3
+#define VCP_USART2_RX_GPIO_Port GPIOA
+#define Led_Pin GPIO_PIN_5
+#define Led_GPIO_Port GPIOA
+#define DEBUG_SWDIO_Pin GPIO_PIN_13
+#define DEBUG_SWDIO_GPIO_Port GPIOA
+#define DEBUG_SWCLK_Pin GPIO_PIN_14
+#define DEBUG_SWCLK_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
