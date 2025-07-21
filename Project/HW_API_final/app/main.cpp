95d2986d7934810996346c5a99fde5391a4024a4
Kristel <jan.kristel@ws-schaefer.com>
2025-07-21 15:23:38 +0200

    project structure

--- Project/HW_API_final/app/main.cpp
+++ Project/HW_API_final/app/main.cpp
@@ -1,4 +1,6 @@
 void main()
 {
-    
+    std::cout << "Starting HW API Test Application..." << std::endl;
+    // print_core_test();
+    print_gpio_test();
 }
---
+++ Project/HW_API_final/app/main.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+* include std headers
+ */
+#include <iostream>
+
+/**
+* include HW_API headers
+ */
+// #include "core_test.hpp"
+#include "gpio_test.hpp"
