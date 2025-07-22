18c890a8b210912ce535588a1530e4d997a9f991
Kristel <jan.kristel@ws-schaefer.com>
2025-07-22 16:30:31 +0200

    esp basic

---
+++ Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Espressif/frameworks/esp-idf-v5.3.1/components/bootloader/subproject"
+  "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader"
+  "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix"
+  "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/tmp"
+  "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/src"
+  "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/jan.kristel/Documents/Thesis/HardwareAPI/Project/ESP32_C6_DevKit_1_blink/blink/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
