f8d29f8d405eaa7e5d215bb62bbacacac1e2e228
Kristel <jan.kristel@ws-schaefer.com>
2025-07-23 12:38:53 +0200

    sample hello world

--- Project/ESP32_C6_DevKitC_sample/main/main.cpp
+++ Project/ESP32_C6_DevKitC_sample/main/main.cpp
@@ -1,6 +1,29 @@
+extern "C" {
 #include <stdio.h>
 
-void app_main(void)
-{
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
 
 }
+
+extern "C"{
+
+    
+    void app_main(void)
+    {
+        printf("Hello world!\n");
+        
+    // for (int i = 10; i >= 0; i--) {
+    //     printf("Restarting in %d seconds...\n", i);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+    fflush(stdout);
+    esp_restart();
+}
+
+} /* extern C */
