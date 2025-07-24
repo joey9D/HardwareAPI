7137ca9159344879e5fae910b410db2918c40424
Kristel <jan.kristel@ws-schaefer.com>
2025-07-24 08:47:40 +0200

    code worked

--- Project/ESP32_C6_DevKitC_sample/main/main.cpp
+++ Project/ESP32_C6_DevKitC_sample/main/main.cpp
@@ -1,97 +1,113 @@
 extern "C" {
 #include <stdio.h>
-
 #include <inttypes.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-// #include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
 #include "driver/gpio.h"
 #include "esp_timer.h"
-
 }
 
 
-extern "C"{
-
-    void toggle_led(bool led_state, int pin)
-    {
-        gpio_num_t gpio_pin = static_cast<gpio_num_t>(pin);
-        if (led_state)
-            gpio_set_level(gpio_pin, !led_state);
-        else
-            gpio_set_level(gpio_pin, led_state);
+class DebounceButton {
+public:
+    DebounceButton(int pin, int64_t debounceTimeUs)
+        : _pin(pin), _debounceTime(debounceTimeUs), _debounceState(0) {
+        _lastTimeUs = 0;
     }
 
-    bool is_button_pressed(int pin)
-    {
-        gpio_num_t gpio_pin = static_cast<gpio_num_t>(pin);
-        static int64_t last_time_us = 0;
-        const int64_t debounce_us = 50000; // 50 ms
+    bool isDebouncePinOn() {
+        bool retval = false;
+        int level = gpio_get_level((gpio_num_t)_pin);
 
-        int level = gpio_get_level(gpio_pin);
-        if (level == 0)  // gedrückt (da Pull-Up aktiviert)
-        {
-            int64_t now = esp_timer_get_time();
-            if ((now - last_time_us) > debounce_us)
-            {
-                last_time_us = now;
-                return true;
+        switch (_debounceState) {
+        case 0: // pin off idle
+            if (level == 0) { // gedrückt (Pull-Up)
+                _lastTimeUs = esp_timer_get_time();
+                _debounceState = 1;
             }
+            break;
+        case 1: // pin off debounce
+            if (level == 0) {
+                if ((esp_timer_get_time() - _lastTimeUs) > _debounceTime) {
+                    _debounceState = 2;
+                }
+            } else {
+                _debounceState = 0;
+            }
+            break;
+        case 2: // pin on idle
+            retval = true;
+            if (level != 0) {
+                _lastTimeUs = esp_timer_get_time();
+                _debounceState = 3;
+            }
+            break;
+        case 3: // pin on debounce
+            retval = true;
+            if (level != 0) {
+                if ((esp_timer_get_time() - _lastTimeUs) > _debounceTime) {
+                    _debounceState = 0;
+                }
+            } else {
+                _debounceState = 2;
+            }
+            break;
+        default:
+            _debounceState = 0;
+            break;
         }
-        return false;
+        return retval;
     }
-    
-    void app_main(void)
-    {
-        printf("Hello world!\n");
 
-        int led_pin = 15;
-        int button_pin = 9;
+private:
+    int _pin;
+    int64_t _debounceTime;
+    int _debounceState;
+    int64_t _lastTimeUs;
+};
 
-        gpio_config_t led_config =
-        {
-            .pin_bit_mask = (1ULL << led_pin), // Pin 15
-            .mode = GPIO_MODE_OUTPUT,
-            .pull_up_en = GPIO_PULLUP_DISABLE,
-            .pull_down_en = GPIO_PULLDOWN_DISABLE,
-            .intr_type = GPIO_INTR_DISABLE
-        };
+extern "C" void app_main(void)
+{
+    printf("Hello world!\n");
 
-        gpio_config_t button_config =
-        {
-            .pin_bit_mask = (1ULL << button_pin), // Pin 9
-            .mode = GPIO_MODE_INPUT,
-            .pull_up_en = GPIO_PULLUP_ENABLE,
-            .pull_down_en = GPIO_PULLDOWN_DISABLE,
-            .intr_type = GPIO_INTR_DISABLE
-        };
+    int led_pin = 15;
+    int button_pin = 9;
 
-        gpio_config(&led_config);
-        gpio_config(&button_config);
+    gpio_config_t led_config = {
+        .pin_bit_mask = (1ULL << led_pin),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
 
-        bool lastButtonState = false;
-        bool led_state = false;
+    gpio_config_t button_config = {
+        .pin_bit_mask = (1ULL << button_pin),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
 
-        while(1) 
-        {
-            bool currentButtonState = is_button_pressed(button_pin);
-            if (!lastButtonState && currentButtonState) 
-            {  
-                toggle_led(led_state, led_pin);
-            }
-            lastButtonState = currentButtonState;
-            // vTaskDelay(pdMS_TO_TICKS(10)); // CPU entlasten
+    gpio_config(&led_config);
+    gpio_config(&button_config);
 
+    DebounceButton button(button_pin, 50000); // 50ms debounce
+
+    bool lastButtonState = button.isDebouncePinOn();
+    bool led_state = false;
+
+    while (1) {
+        bool currentButtonState = button.isDebouncePinOn();
+
+        if (!lastButtonState && currentButtonState) {
+            led_state = !led_state;
+            gpio_set_level((gpio_num_t)led_pin, led_state);
         }
-    // for (int i = 10; i >= 0; i--) {
-    //     printf("Restarting in %d seconds...\n", i);
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    // }
+        lastButtonState = currentButtonState;
+        vTaskDelay(pdMS_TO_TICKS(10)); // CPU entlasten
+    }
     fflush(stdout);
     // esp_restart();
 }
-
-} /* extern C */
